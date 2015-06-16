9e82ca9e89fcb763cde669d750483ceef86a7b96
Witold Skibniewski <witold.skibniewski@tivix.com>
2015-06-16 10:42:31 +0200

    initial commit

---
+++ CITest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CITest
+//
+//  Created by WITOLD SKIBNIEWSKI on 16/06/15.
+//  Copyright (c) 2015 WITOLD SKIBNIEWSKI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
