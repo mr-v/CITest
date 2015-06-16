a3a727bec6605434e567ce2e1c31b9a60c990dc9
Witold Skibniewski <witold.skibniewski@tivix.com>
2015-06-16 10:44:47 +0200

    add cocoa pods (creates workspace)

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+use_frameworks!
+
+target 'CITest' do
+pod 'Moya'
+end
+
+target 'CITestTests' do
+
+end
+
---
+++ Pods/Target Support Files/Pods-CITest/Pods-CITest-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Alamofire
+#define COCOAPODS_POD_AVAILABLE_Alamofire
+#define COCOAPODS_VERSION_MAJOR_Alamofire 1
+#define COCOAPODS_VERSION_MINOR_Alamofire 2
+#define COCOAPODS_VERSION_PATCH_Alamofire 3
+
+// Moya
+#define COCOAPODS_POD_AVAILABLE_Moya
+#define COCOAPODS_VERSION_MAJOR_Moya 1
+#define COCOAPODS_VERSION_MINOR_Moya 1
+#define COCOAPODS_VERSION_PATCH_Moya 1
+
+// Moya/Core
+#define COCOAPODS_POD_AVAILABLE_Moya_Core
+#define COCOAPODS_VERSION_MAJOR_Moya_Core 1
+#define COCOAPODS_VERSION_MINOR_Moya_Core 1
+#define COCOAPODS_VERSION_PATCH_Moya_Core 1
+
