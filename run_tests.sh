4dd801660bb5bbdde6ffdc8bc3c864f238fc8311
Witold Skibniewski <witold.skibniewski@tivix.com>
2015-06-16 12:45:37 +0200

    run tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 set -e
 #xcodebuild -workspace CITest.xcworkspace/ -scheme CITest -configuration Release -sdk iphonesimulator test
-xctool test -workspace CITest.xcworkspace/ -scheme CITest -sdk iphonesimulator -reporter junit:test-reports/junit-report.xml
+xctool run-tests -workspace CITest.xcworkspace/ -scheme CITest -sdk iphonesimulator -reporter junit:test-reports/junit-report.xml
