a5916376f7c9a828bfdf9910fd0ccf2bb7dc708c
Witold Skibniewski <witold.skibniewski@tivix.com>
2015-06-16 11:31:03 +0200

    update run test

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,2 @@
-xcodebuild -workspace CITest.xcworkspace/ -scheme CITest -configuration Release -sdk iphonesimulator clean test
+set -e
+xcodebuild -workspace CITest.xcworkspace/ -scheme CITest -configuration Release -sdk iphonesimulator test
