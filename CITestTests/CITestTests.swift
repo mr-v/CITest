95ff8fc8ba159eb6f23bd7a3d8cd5ad92181a8ab
Witold Skibniewski <witold.skibniewski@tivix.com>
2015-06-16 11:09:31 +0200

    add failing test

--- CITestTests/CITestTests.swift
+++ CITestTests/CITestTests.swift
@@ -23,14 +23,7 @@ class CITestTests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        XCTFail("failing test")
+//        XCTAssert(true, "Pass")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
